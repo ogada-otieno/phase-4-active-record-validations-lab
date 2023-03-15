57a8096bff32341428117c89bbc97a3c44d7ad95
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-15 10:10:38 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    
+    # Post category is either Fiction or Non-Fiction
+    # This step requires an inclusion validator
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :not_allow_true_facts
+
+    # Custom validations: is expected not to allow :title to be ‹"True Facts"›
+
+    def not_allow_true_facts
+        if title == "True Facts"
+            errors.add(:title, "Cannot be 'True Facts'")
+        end
+    end
+
 end
